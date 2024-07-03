89b1a2612859f6dd4981f1426a7973e5378634b0
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-03 23:22:33 +0530

    boiler plate created in TaskCard.jsx file

---
+++ src/assets/components/TaskCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./TaskCard.css";
+
+const TaskCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TaskCard
--- src/assets/components/TaskColumn.jsx
+++ src/assets/components/TaskColumn.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./TaskColumn.css";
 
-const TaskColumn = (section) => {
+const TaskColumn = (section) => { //props function
   return (
     <section className="task_column">
       <h2>{section.sectionName}</h2>
