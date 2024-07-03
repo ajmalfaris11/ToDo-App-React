8c8158e84edf0ecc60803305653effdc4559c99f
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-03 22:11:13 +0530

    header section designed

--- src/assets/components/TaskForm.jsx
+++ src/assets/components/TaskForm.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
+import React from "react";
+import "./TaskForm.css";
 
 const TaskForm = () => {
   return (
     <header className="app_header">
-        <form action="">
-            <input type="text" className='task_input' 
-            placeholder='Enter Your Task'/>
+      <form action="">
+        <input
+          type="text"
+          className="task_input"
+          placeholder="Enter Your Task"
+        />
 
-            <div className="task_form_buttom_line">
-                <button className="tag">HTML</button>
-                <button className="tag">CSS</button>
-                <button className="tag">JavaScript</button>
-                <button className="tag">React</button>
+        <div className="task_form_buttom_line">
+          <div>
+            <button className="tag">HTML</button>
+            <button className="tag">CSS</button>
+            <button className="tag">JavaScript</button>
+            <button className="tag">React</button>
+          </div>
 
-                <select name="" id="" className="task_Status">
-                    <option value="todo">To do</option>
-                    <option value="doing">Doing</option>
-                    <option value="done">Done</option>
-                </select>
+          <div>
+            <select name="" id="" className="task_status">
+              <option value="todo">To do</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
 
-                <button type='submit' 
-                className='task_submit'>
-                    + Add Task
-                </button>
-            </div>
-        </form>
+            <button type="submit" className="task_submit">
+              + Add Task
+            </button>
+          </div>
+        </div>
+      </form>
     </header>
-  )
-}
+  );
+};
 
-export default TaskForm
+export default TaskForm;
