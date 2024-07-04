1984d0d05a4f6944e5d6722d723e09801c00f201
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-04 12:29:25 +0530

    designed task card

--- src/assets/components/TaskCard.jsx
+++ src/assets/components/TaskCard.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
 import "./TaskCard.css";
+import Tag from "./Tag";
+
 
 const TaskCard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default TaskCard
+      <article className="task_card">
+        <p className="task_text">This is Samble Text</p>
+
+        <div className="task_card_bottom_line">
+          <div className="task_card_tags">
+            <Tag tagName="HTML" />
+            <Tag tagName="CSS" />
+          </div>
+          <div className="task_delete">
+            <span class="material-symbols-outlined">delete</span>
+          </div>
+        </div>
+      </article>
+
+  );
+};
+
+export default TaskCard;
--- src/assets/components/TaskColumn.jsx
+++ src/assets/components/TaskColumn.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
 
 const TaskColumn = (section) => { //props function
   return (
     <section className="task_column">
       <h2>{section.sectionName}</h2>
+      <TaskCard />
     </section>
   );
 };
