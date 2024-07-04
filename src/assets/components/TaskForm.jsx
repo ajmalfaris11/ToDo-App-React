b5059bbf1f151793ff3651041bd4b5d4aeb8c6bd
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-07-04 23:18:17 +0530

    created usestate variables and created functions for take input from task and status

--- src/assets/components/TaskForm.jsx
+++ src/assets/components/TaskForm.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+  // create state variable
+  const [task, setTask] = useState("");
+  const [status, setStatus] = useState("todo");
+
+  // function for take the task input 
+  const handleTaskChange = e => {
+    setTask(e.target.value);
+  }
+
+    // function for take the status input 
+  const handleStatusChange = e => {
+    setStatus(e.target.value);
+  }
+
+  console.log(task, status )
+
   return (
     <header className="app_header">
       <form action="">
@@ -10,6 +26,7 @@ const TaskForm = () => {
           type="text"
           className="task_input"
           placeholder="Enter Your Task..."
+          onChange={ handleTaskChange }
         />
 
         <div className="task_form_buttom_line">
@@ -22,7 +39,8 @@ const TaskForm = () => {
           </div>
 
           <div>
-            <select name="" id="" className="task_status">
+            <select name="" id="" className="task_status"
+            onChange={handleStatusChange}>
               <option value="todo">To do</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
